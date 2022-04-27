6e8e2f90cf6895b776dcfb90f381e02c5bd7631d
AbdrashitovaYuliya <juli.lopatkina@gmail.com>
2022-04-27 20:08:47 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 2000;
+        int oneMile = 20;
+        int totalMile = amount / oneMile;
+        System.out.println("У Вас " + totalMile + " миль");
+
+    }
+}
